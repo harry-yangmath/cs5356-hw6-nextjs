fa0fe8507f70e28d8beb643e975a7534da2f1655
Harry Yang <56176392+harry-yangmath@users.noreply.github.com>
2025-04-08 14:02:01 -0400

    change

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    experimental: {
-        nodeMiddleware: true,
-    },
+  output: 'standalone',
+  experimental: {
+    nodeMiddleware: true,
+    serverActions: true,
+  },
+  images: {
+    domains: ['localhost', 'vercel.app']
+  }
 }
 
 export default nextConfig
